01c770e65db200521febe7296e2b4bea4c66110d
Sara Strong <sara.n.strong@gmail.com>
2020-07-27 21:54:19 -0400

    Part One of Assignment

---
+++ python-syntax/any7.py
@@ -0,0 +1,18 @@
+def any7(nums):
+    """Are any of these numbers a 7? (True/False)"""
+
+    # YOUR CODE HERE 
+    result = None
+    for i in nums:
+        if i==7:
+            result = True
+            break
+        else:
+            result = False
+    return result
+
+        
+
+print("should be true", any7([1, 2, 7, 4, 5]))
+print("should be false", any7([1, 2, 4, 5]))
+
