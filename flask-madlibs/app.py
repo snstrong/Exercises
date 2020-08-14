8b76c3b598797e9c5c1b79d80bc5460b2bd90567
Sara Strong <sara.n.strong@gmail.com>
2020-08-14 19:48:48 -0400

    Added files

---
+++ flask-madlibs/app.py
@@ -0,0 +1,7 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+debug = DebugToolbarExtension(app)
