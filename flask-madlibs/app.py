a729de2edd648c23c4d09652b0b2b79ab5aaf7ac
Sara Strong <sara.n.strong@gmail.com>
2020-08-14 21:12:28 -0400

    Assignment completed

--- flask-madlibs/app.py
+++ flask-madlibs/app.py
@@ -1,7 +1,17 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+import stories
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def get_answers():
+    return render_template('index.html', prompts = stories.story.prompts)
+
+@app.route('/your_story')
+def make_story():
+    generated_story = stories.story.generate(request.args)
+    return render_template('your_story.html', your_story=generated_story)
