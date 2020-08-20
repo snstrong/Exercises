396cc9645e82c751f78186811c0664c1886e8bdb
Sara Strong <sara.n.strong@gmail.com>
2020-08-19 21:17:36 -0400

    Added start page

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,8 +1,20 @@
 from flask import Flask, request, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from random import randint, choice, sample
-#
+import surveys
+# from random import randint, choice, sample
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
+
+#############################################################
+
+responses = []
+
+@app.route('/')
+def get_start_page():
+    """Shows start page with survey title, instructions, and start button"""
+    return render_template('index.html', title=surveys.satisfaction_survey.title, instructions=surveys.satisfaction_survey.instructions)
+
+
