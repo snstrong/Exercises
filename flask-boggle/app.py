a223ad084f953dec485464b51a0b8d7be1947c2c
Sara Strong <sara.n.strong@gmail.com>
2020-08-25 17:25:59 -0400

    Displays table, added styles

--- flask-boggle/app.py
+++ flask-boggle/app.py
@@ -2,8 +2,14 @@ from boggle import Boggle
 from flask import Flask, request, render_template, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
 boggle_game = Boggle()
 
 @app.route('/')
 def show_board():
-    return render_template('index.html')
+    board = boggle_game.make_board()
+    return render_template('index.html', board=board)
