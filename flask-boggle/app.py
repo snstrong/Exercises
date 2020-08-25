d2c6d7bf7ee6951f074c84478d702e0e593862e8
Sara Strong <sara.n.strong@gmail.com>
2020-08-25 15:39:44 -0400

    File setup

--- flask-boggle/app.py
+++ flask-boggle/app.py
@@ -1,3 +1,9 @@
 from boggle import Boggle
+from flask import Flask, request, render_template, flash, redirect, session
+from flask_debugtoolbar import DebugToolbarExtension
 
 boggle_game = Boggle()
+
+@app.route('/')
+def show_board():
+    return render_template('index.html')
