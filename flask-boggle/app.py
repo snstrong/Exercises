5bdea39b2b97c538bc2b241f9be03611383db758
Sara Strong <sara.n.strong@gmail.com>
2020-08-27 21:14:41 -0400

    Updated and added functionality

--- flask-boggle/app.py
+++ flask-boggle/app.py
@@ -1,5 +1,7 @@
+"""Endpoints for Boggle game"""
+
 from boggle import Boggle
-from flask import Flask, request, render_template, flash, redirect, session, jsonify
+from flask import Flask, request, render_template, redirect, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -10,18 +12,40 @@ debug = DebugToolbarExtension(app)
 boggle_game = Boggle()
 
 @app.route('/')
-def show_board():
+def start_game():
+    """starts game"""
     board = boggle_game.make_board()
     session['board'] = board
     session['score'] = 0
+    if not session.get('visits'):
+        session['visits'] = 0
+    visits = session['visits'];
+    visits += 1
+    session['visits'] = visits
     return render_template('index.html', board=board)
 
 @app.route('/guess', methods=['GET'])
 def check_guess():
+    """checks to see if guessed word is valid and updates score"""
     word = request.args['guess']
     board = session['board']
     score = session['score']
     response = boggle_game.check_valid_word(board, word)
     if response == "ok":
         session['score'] = score + len(word)
-    return jsonify({'result': response, 'score': session['score']})
+    return jsonify({'result': response, 'score': session['score'], 'points': len(word)})
+
+@app.route('/end_game', methods=['GET'])
+def end_game():
+    """updates and sends back list of high scores at end of game"""    
+    scores_list = []
+    if not session.get('scores_list'):
+        session['scores_list'] = [session['score']]
+        scores_list = session['scores_list']
+    else:
+        scores_list = session['scores_list']
+        scores_list.append(session['score'])
+        scores_list.sort(reverse=True)
+        session['scores_list'] = scores_list
+    return jsonify({'scores_list': session['scores_list']})
+    
--- flask-boggle/static/app.js
+++ flask-boggle/static/app.js
@@ -1,25 +1,74 @@
-$guessForm = $('#guess-form')
+$guessForm = $("#guess-form");
+let guessedArr = [];
+let currentScore = 0;
 
-$guessForm.on('submit', async function(evt) {
-    evt.preventDefault();
-    $guessInput = $('#guess');
-    response = await axios.get('/guess', {
-        params: 
-        {
-            guess: $('#guess').val()
-        }
+$guessForm.on("submit", async function (evt) {
+  evt.preventDefault();
+  let $guessInput = $("#guess");
+  let word = $("#guess").val();
+  let response = await checkWord(word);
+  if (response === "Can't guess same word twice") {
+    console.log(response);
+  } else {
+    guessedArr.push(word);
+    $("#score").text(`Score: ${response.data.score}`).addClass("score");
+    addCheckedWord(word, response.data.result);
+    currentScore = response.data.score;
+  }
+  $guessInput.val("").focus();
+  return false;
+});
+
+function addCheckedWord(word, responseClass) {
+    $("#guessed-words").append(
+        `<span class="${responseClass} guessed-word">${word}</span>`
+    );
+    return true;
+}
+
+async function checkWord(word) {
+  let response;
+  if (guessedArr.includes(word)) {
+    response = "Can't guess same word twice";
+  } else {
+    guessedArr.push(word);
+    response = await axios.get("/guess", {
+      params: {
+        guess: word,
+      },
     });
-    let responseClass;
-    if (response.data.result === "ok") {
-        responseClass = "ok"
+  }
+  return response;
+}
+
+let seconds = 60;
+let timer = setInterval(countdown, 1000);
+function countdown() {
+  seconds -= 1;
+  $("#timer").text(`0:${seconds}`);
+  if (seconds === 0) {
+    clearInterval(timer);
+    alert("GAME OVER!");
+    $guessForm.hide();
+    endGame();
+  }
+}
+
+async function endGame() {
+  let response = await axios.get("/end_game");
+  console.log(response);
+  let listLength;
+  response.data.scores_list.length <= 10 ? listLength = response.data.scores_list.length : listLength = 10;
+  for (let i = 0; i < listLength; i++) {
+    // for (let score in response.data.scores_list) {
+    // console.log(response.data.scores_list[i]);
+    let classes = "listed-score";
+    if (response.data.scores_list[i] === currentScore) {
+        classes = `${classes} current-score"`
     }
-    else if (response.data.result === "not-on-board") {
-        responseClass = "not-on-board"
-    }
-    else if (response.data.result === "not-word") {
-        responseClass = "not-word"
-    }
-    $('#guessed-words').append(`<span class="${responseClass} guessed-word">${$('#guess').val()}</span>`);
-    $guessInput.val("").focus();
-    return false;
-})
+    $('#scores-list').append(`<li><span class="${classes}">${response.data.scores_list[i]}</span></li>`)
+  }
+  $('#end-game').show();
+}
+$('#end-game').hide();
+countdown();
