23603d5b52a52d354c78250ef216e9c25123f73d
Sara Strong <sara.n.strong@gmail.com>
2020-09-20 14:42:33 -0400

    2.1 completed

--- sql-ddl-design/outer_space.sql
+++ sql-ddl-design/outer_space.sql
@@ -1,5 +1,5 @@
--- from the terminal run:
--- psql < outer_space.sql
+-- Data Modeling Exercise: Schema Critique
+-- Part One: Outer Space
 
 DROP DATABASE IF EXISTS outer_space;
 
@@ -7,22 +7,80 @@ CREATE DATABASE outer_space;
 
 \c outer_space
 
+-- Given code to be revised:
+--
+-- CREATE TABLE planets
+-- (
+--   id SERIAL PRIMARY KEY,
+--   name TEXT NOT NULL,
+--   orbital_period_in_years FLOAT NOT NULL,
+--   orbits_around TEXT NOT NULL,
+--   galaxy TEXT NOT NULL,
+--   moons TEXT[]
+-- );
+
+-- INSERT INTO planets
+--   (name, orbital_period_in_years, orbits_around, galaxy, moons)
+-- VALUES
+--   ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
+--   ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
+--   ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
+--   ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
+--   ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
+--   ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+
+-------------------------------------------------------------------------------
+
+-- Revision:
+--
 CREATE TABLE planets
 (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   orbital_period_in_years FLOAT NOT NULL,
   orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  galaxy TEXT NOT NULL
+);
+
+CREATE TABLE moons
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  orbits_around INT NOT NULL REFERENCES planets
 );
 
 INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
+  (name, orbital_period_in_years, orbits_around, galaxy)
 VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+  ('Earth', 1.00, 'The Sun', 'Milky Way'),
+  ('Mars', 1.88, 'The Sun', 'Milky Way'),
+  ('Venus', 0.62, 'The Sun', 'Milky Way'),
+  ('Neptune', 164.8, 'The Sun', 'Milky Way'),
+  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way'),
+  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way');
+
+INSERT INTO moons
+  (name, orbits_around)
+VALUES
+  ('The Moon', 1),
+  ('Phobos', 2),
+  ('Deimos', 2),
+  -- 4: {"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}
+  -- Is there a way to do this with a loop?
+  ('Naiad', 4),
+  ('Thalassa', 4),
+  ('Despina', 4),
+  ('Galatea', 4),
+  ('Larissa', 4),
+  ('S/2004 N 1', 4),
+  ('Proteus', 4),
+  ('Triton', 4),
+  ('Nereid', 4),
+  ('Halimede', 4),
+  ('Sao', 4),
+  ('Laomedeia', 4),
+  ('Psamathe', 4),
+  ('Neso', 4);
+
+  -- Get planets and their corresponding moons:
+  SELECT planets.name AS Planet, moons.name AS Moon FROM planets LEFT JOIN moons ON planets.id = moons.orbits_around;
