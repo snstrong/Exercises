3dd6605ee0ffb68c62e5228b3e29c99c5ae1fbe6
Sara Strong <sara.n.strong@gmail.com>
2020-10-27 21:38:46 -0400

    User registration functionality added

---
+++ flask-feedback/app.py
@@ -0,0 +1,83 @@
+"""Flask Feedback App"""
+from flask import Flask, render_template, redirect, session, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, db, User
+# from forms import UserForm, TweetForm
+from sqlalchemy.exc import IntegrityError
+from forms import RegisterForm, LoginForm
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///feedback"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "abc123"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+connect_db(app)
+toolbar = DebugToolbarExtension(app)
+
+@app.route('/')
+def redirect_to_register():
+    return redirect('/register')
+
+# @app.route('/register')
+# def show_registration_form():
+#     form = RegisterForm()
+#     return render_template('/register.html', form=form)
+
+@app.route('/register', methods=['GET','POST'])
+def process_registration():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        new_user = User.register(username, password, email, first_name, last_name)
+        db.session.add(new_user)
+        db.session.commit()
+        session['user_id'] = new_user.username
+        flash('Welcome! Successfully Created Your Account!', "success")
+        return redirect('/secret')
+    return render_template('/register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def show_login_form():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        user = User.authenticate(username, password)
+        if user:
+            session['user_id'] = username
+            flash(f"Welcome Back, {user.username}!")
+            return redirect('/secret')
+        else:
+            form.username.errors = ['Invalid username/password.']
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_user():
+    session.pop('user_id')
+    flash("Goodbye!")
+    return redirect('/login')
+
+@app.route('/secret')
+def show_secret():
+    if "user_id" not in session:
+        flash("Please login first!", "danger")
+        return redirect('/login')
+    else:
+        return "You made it!"
+
+@app.route('/users/<username>')
+def show_user_info(username):
+    if "user_id" not in session:
+        flash("Please login first!", "danger")
+        return redirect('/login')
+    elif session["user_id"] != username:
+        flash("You are not authorized to view that page!", "danger")
+        return redirect(f'/users/{session["user_id"]}')
+    user = User.query.get_or_404(username)
+    return render_template('user_info.html', user=user)
---
+++ flask-feedback/forms.py
@@ -0,0 +1,44 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, BooleanField, DateField, DateTimeField, IntegerField, TextAreaField, SelectField, PasswordField
+from wtforms.fields.html5 import URLField, EmailField
+from wtforms.validators import InputRequired, Optional, URL, Length
+class RegisterForm(FlaskForm):
+    """Form for registering a new user."""
+
+    username = StringField(
+        "Username",
+        validators=[InputRequired(message="Username required"), Length(max=20)]
+    )
+
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(message="Password required")]
+    )
+
+    email = EmailField(
+        "Email",
+        validators=[InputRequired(message="Email required"), Length(max=50)]
+    )
+
+    first_name = StringField(
+        "First Name",
+        validators=[InputRequired(message="First name required"), Length(max=30)]
+    )
+
+    last_name = StringField(
+        "Last Name",
+        validators=[InputRequired(message="Last name required"), Length(max=30)]
+    )
+
+class LoginForm(FlaskForm):
+    """Form for logging in a user."""
+
+    username = StringField(
+        "Username",
+        validators=[InputRequired(message="Username required")]
+    )
+
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(message="Password required")]
+    )
