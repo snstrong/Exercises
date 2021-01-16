c80dd5089016881cd2398b4a697fb1c07506bc28
Sara Strong <sara.n.strong@gmail.com>
2021-01-15 22:48:00 -0500

    step 1 complete

---
+++ node-files/step1.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+function cat(path) {
+  fs.readFile(path, "utf8", function (err, data) {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    console.log(`${data}`);
+  });
+}
+
+cat("dickinson.txt");
+
+// Below line will throw error:
+// cat("nonexistent.txt");
