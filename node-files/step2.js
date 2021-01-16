46c59d7adaa3a26a0ccc33fbbdcb7bb3204c779c
Sara Strong <sara.n.strong@gmail.com>
2021-01-15 23:00:56 -0500

    Added webCat fn

---
+++ node-files/step2.js
@@ -0,0 +1,34 @@
+// Add a new function, webCat. This should take a URL and, using axios, should read the content of that URL and print it to the console.
+
+// Modify the code that invoked cat so that, based on the command-line args, it decides whether the argument is a file path or a URL and calls either cat or webCat, respectively.
+
+const fs = require("fs");
+const axios = require("axios");
+
+function cat(path) {
+  fs.readFile(path, "utf8", function (err, data) {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    console.log(`${data}`);
+  });
+}
+
+function webCat(url) {
+  try {
+    axios.get(url).then(function (resp) {
+      console.log(resp.data.slice(0, 80), "...");
+    });
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+cat("dickinson.txt");
+
+webCat("http://www.google.com");
+
+// Below line will throw error:
+// cat("nonexistent.txt");
