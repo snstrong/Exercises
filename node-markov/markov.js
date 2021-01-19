2d019a6fa1b4cae183638ce99a451e6604070fe4
Sara Strong <sara.n.strong@gmail.com>
2021-01-19 17:31:36 -0500

    makeChains

--- node-markov/markov.js
+++ node-markov/markov.js
@@ -1,13 +1,13 @@
 /** Textual markov chain generator */
 
+const fs = require("fs");
 
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
+    this.words = words.filter((c) => c !== "");
     this.makeChains();
   }
 
@@ -17,13 +17,34 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    // let
+    let result = {};
+    for (let i = 0; i < this.words.length; i++) {
+      let next;
+      if (this.words[i + 1]) {
+        next = this.words[i + 1];
+      } else {
+        next = null;
+      }
+      if (result && this.words[i] in result) {
+        result[this.words[i]].push(next);
+      } else {
+        result[this.words[i]] = [];
+        result[this.words[i]].push(next);
+      }
+    }
+    console.log(result);
+    return result;
   }
-
 
   /** return random text from chains */
-
   makeText(numWords = 100) {
     // TODO
+    let firstWord = this.words[Math.floor(Math.random() * this.words.length)];
+    console.log(firstWord);
   }
 }
+
+let m = new MarkovMachine(
+  "Shall I compare thee to a summer’s day? Thou art more lovely and more temperate: Rough winds do shake the darling buds of May, And summer’s lease hath all too short a date."
+);
