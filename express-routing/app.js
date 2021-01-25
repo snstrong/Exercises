a407dcff5382a43b99974af5dff6a1d786ef05d0
Sara Strong <sara.n.strong@gmail.com>
2021-01-25 18:08:19 -0500

    Mean route

--- express-routing/app.js
+++ express-routing/app.js
@@ -5,66 +5,62 @@ const app = express();
 app.use(express.json());
 
 function calculateMean(nums) {
-    let total;
-    for (let num in nums) {
-        total = total + num;
-    }
-    return total / nums.length - 1;
+  let total = 0;
+  for (let num of nums) {
+    total = total + num;
+  }
+  return total / nums.length;
 }
 
 function calculateMedian(nums) {
-    // https://www.tutorialspoint.com/calculating-median-of-an-array-javascript
-    
-    const sorted = arr.slice().sort((a, b) => {
-        return a - b;
-     });
-     if(sorted.length % 2 === 0){
-        const first = sorted[sorted.length / 2 - 1];
-        const second = sorted[sorted.length / 2];
-        return (first + second) / 2;
-     }
-     else{
-        const mid = Math.floor(sorted.length / 2);
-        return sorted[mid];
-     };
+  // https://www.tutorialspoint.com/calculating-median-of-an-array-javascript
+
+  const sorted = arr.slice().sort((a, b) => {
+    return a - b;
+  });
+  if (sorted.length % 2 === 0) {
+    const first = sorted[sorted.length / 2 - 1];
+    const second = sorted[sorted.length / 2];
+    return (first + second) / 2;
+  } else {
+    const mid = Math.floor(sorted.length / 2);
+    return sorted[mid];
+  }
 }
 
 function calculateMode(nums) {
-    // Logic: https://www.mathsisfun.com/mode.html
+  // See https://jonlabelle.com/snippets/view/javascript/calculate-mean-median-mode-and-range-in-javascript
+}
 
+let testNums = "1,2,3,4,5";
+
+function getQueryInts(nums) {
+  let arr = nums.split(",");
+  console.log(arr);
+  arr.forEach((num, index, arr) => {
+    arr[index] = parseInt(num);
+  });
+  return arr;
 }
 
 app.get("/mean", (req, res) => {
-    // res.json(CANDIES);
-  });
+  // res.json(CANDIES);
+  let nums = req.query.nums;
+  let arr = getQueryInts(nums);
+  console.log(arr);
+  let mean = calculateMean(arr);
+  console.log("The mean of these numbers is: ", mean);
+  res.send("This is the MEAN route");
+});
 app.get("/median", (req, res) => {
-    // res.json(CANDIES);
-  });
+  // res.json(CANDIES);
+  res.send("This is the MEDIAN route");
+});
 app.get("/mode", (req, res) => {
-    // res.json(CANDIES);
-  });
+  // res.json(CANDIES);
+  res.send("This is the MODE route");
+});
 
-
-
-// const CANDIES = [
-//   { name: "snickers", qty: 43, price: 1.5 },
-//   { name: "skittles", qty: 26, price: 0.99 },
-// ];
-
-// app.get("/candies", (req, res) => {
-//   res.json(CANDIES);
-// });
-
-// app.post("/candies", (req, res) => {
-//   if (req.body.name.toLowerCase() === "circus peanuts") {
-//     res
-//       .status(403)
-//       .json({ msg: "HORRIBLE CHOICE.  CIRCUS PEANUTS FORBIDDEN!" });
-//   }
-//   CANDIES.push(req.body);
-//   res.status(201).json(CANDIES);
-// });
-
-// app.listen(3000, () => {
-//   console.log("Server running on port 3000");
+app.listen(3000, function () {
+  console.log("App on port 3000");
 });
