19b3a817dee998e03fe9ac0f66812ed00801cd02
Sara Strong <sara.n.strong@gmail.com>
2021-01-28 21:47:53 -0500

    Shopping list items

---
+++ express-shopping-list/fakeDb.js
@@ -0,0 +1,13 @@
+global.items = [
+  { name: "blueberries", price: 10.99 },
+  { name: "popsicles", price: 4.99 },
+  { name: "frittatas", price: 6.99 },
+  { name: "whole grain bread", price: 3.99 },
+  { name: "olive oil", price: 10.99 },
+  { name: "toilet paper", price: 7.99 },
+  { name: "broccoli", price: 2.99 },
+  { name: "whole milk", price: 3.99 },
+  { name: "brownie bites", price: 4.99 },
+];
+
+module.exports = items;
