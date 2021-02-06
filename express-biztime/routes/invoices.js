0e63fc5ba06c966e57382eb9980cf7ffd9b3058f
Sara Strong <sara.n.strong@gmail.com>
2021-02-05 21:38:51 -0500

    Updated put route - payment dates

--- express-biztime/routes/invoices.js
+++ express-biztime/routes/invoices.js
@@ -45,14 +45,35 @@ router.post("/", async (req, res, next) => {
 router.put("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
-    const { amt } = req.body;
-    const results = await db.query(
-      "UPDATE invoices SET amt=$1 WHERE id=$2 RETURNING id, comp_code, amt, paid, add_date, paid_date",
-      [amt, id]
+    const { amt, paid } = req.body;
+
+    // Query database for current record of payment or non-payment paid status
+    const record = await db.query(
+      "SELECT paid, paid_date FROM invoices WHERE id=$1",
+      [id]
     );
+
+    let paid_date;
+    if (paid) {
+      if (!record.rows[0].paid_date) {
+        paid_date = new Date();
+      } else {
+        paid_date = current.rows[0].paid_date;
+      }
+    } else {
+      paid_date = null;
+    }
+
+    // Update record
+    const results = await db.query(
+      "UPDATE invoices SET amt=$1, paid=$2, paid_date=$3 WHERE id=$4 RETURNING id, comp_code, amt, paid, add_date, paid_date",
+      [amt, paid, paid_date, id]
+    );
+
     if (results.rows.length === 0) {
       throw new ExpressError(`Invoice with id ${id} not found`, 404);
     }
+
     return res.json({ invoice: results.rows[0] });
   } catch (err) {
     return next(err);
