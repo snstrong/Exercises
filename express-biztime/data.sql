4d97e8664d9e355a6b943cb012db2a81e586da13
Sara Strong <sara.n.strong@gmail.com>
2021-02-05 22:44:58 -0500

    industries table; companies_industries join table

--- express-biztime/data.sql
+++ express-biztime/data.sql
@@ -1,7 +1,9 @@
 \c biztime
 
+DROP TABLE IF EXISTS companies_industries;
 DROP TABLE IF EXISTS invoices;
 DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industries;
 
 CREATE TABLE companies (
     code text PRIMARY KEY,
@@ -19,6 +21,17 @@ CREATE TABLE invoices (
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
 
+CREATE TABLE industries (
+  code text PRIMARY KEY,
+  industry text NOT NULL UNIQUE
+);
+
+CREATE TABLE companies_industries (
+  company_code text NOT NULL REFERENCES companies ON DELETE CASCADE,
+  industry_code text NOT NULL REFERENCES industries ON DELETE CASCADE,
+  PRIMARY KEY(company_code, industry_code)
+);
+
 INSERT INTO companies
   VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
          ('ibm', 'IBM', 'Big blue.');
@@ -28,3 +41,15 @@ INSERT INTO invoices (comp_Code, amt, paid, paid_date)
          ('apple', 200, false, null),
          ('apple', 300, true, '2018-01-01'),
          ('ibm', 400, false, null);
+
+INSERT INTO industries
+  VALUES
+    ('tech', 'Technology'),
+    ('manu', 'Manufacturing');
+
+INSERT INTO companies_industries
+  VALUES
+    ('apple', 'tech'),
+    ('apple', 'manu'),
+    ('ibm', 'tech'),
+    ('ibm', 'manu');
