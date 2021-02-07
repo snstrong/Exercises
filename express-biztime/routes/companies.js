6bcf43391c786570d033b719fd273506ee7506a5
Sara Strong <sara.n.strong@gmail.com>
2021-02-06 22:26:12 -0500

    GET company shows associated industries

--- express-biztime/routes/companies.js
+++ express-biztime/routes/companies.js
@@ -18,20 +18,34 @@ router.get("/", async (req, res, next) => {
 router.get("/:code", async (req, res, next) => {
   try {
     const { code } = req.params;
-    const results = await db.query(`SELECT * FROM companies WHERE code = $1`, [
-      code,
-    ]);
+    const results = await db.query(
+      `SELECT c.code, c.name, c.description, i.industry 
+      FROM companies AS c
+      LEFT JOIN companies_industries AS c_i 
+      ON c.code = c_i.company_code 
+      LEFT JOIN industries AS i 
+      ON c_i.industry_code = i.code 
+      WHERE c.code=$1;`,
+      [code]
+    );
     if (results.rows.length === 0) {
       throw new ExpressError(`Can't find company with code ${code}`, 404);
     }
-    const invoices = await db.query(
+    const invoice_results = await db.query(
       "SELECT * FROM invoices WHERE comp_code=$1",
       [code]
     );
-    const companyData = results.rows[0];
-    companyData.invoices = invoices.rows;
+    const { c_code, name, description } = results.rows[0];
+    const invoices = invoice_results.rows;
+    const industries = results.rows.map((r) => r.industry);
 
-    return res.json({ company: companyData });
+    return res.json({
+      company: c_code,
+      name,
+      description,
+      industries,
+      invoices,
+    });
   } catch (err) {
     return next(err);
   }
