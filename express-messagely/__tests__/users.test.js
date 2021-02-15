209ee94553826681751f1d16f02f7a3fbab99b3c
Sara Strong <sara.n.strong@gmail.com>
2021-02-15 14:59:58 -0500

    Test for messagesFrom

--- express-messagely/__tests__/users.test.js
+++ express-messagely/__tests__/users.test.js
@@ -71,70 +71,72 @@ describe("Test User class", function () {
   });
 });
 
-// describe("Test messages part of User class", function () {
-//   beforeEach(async function () {
-//     await db.query("DELETE FROM messages");
-//     await db.query("DELETE FROM users");
-//     await db.query("ALTER SEQUENCE messages_id_seq RESTART WITH 1");
+describe("Test messages part of User class", function () {
+  beforeEach(async function () {
+    await db.query("DELETE FROM messages");
+    await db.query("DELETE FROM users");
+    await db.query("ALTER SEQUENCE messages_id_seq RESTART WITH 1");
 
-//     let u1 = await User.register({
-//       username: "test1",
-//       password: "password",
-//       first_name: "Test1",
-//       last_name: "Testy1",
-//       phone: "+14155550000",
-//     });
-//     let u2 = await User.register({
-//       username: "test2",
-//       password: "password",
-//       first_name: "Test2",
-//       last_name: "Testy2",
-//       phone: "+14155552222",
-//     });
-//     let m1 = await Message.create({
-//       from_username: "test1",
-//       to_username: "test2",
-//       body: "u1-to-u2"
-//     });
-//     let m2 = await Message.create({
-//       from_username: "test2",
-//       to_username: "test1",
-//       body: "u2-to-u1"
-//     });
-//   });
+    let u1 = await User.register({
+      username: "test1",
+      password: "password",
+      first_name: "Test1",
+      last_name: "Testy1",
+      phone: "+14155550000",
+    });
+    let u2 = await User.register({
+      username: "test2",
+      password: "password",
+      first_name: "Test2",
+      last_name: "Testy2",
+      phone: "+14155552222",
+    });
+    let m1 = await Message.create({
+      from_username: "test1",
+      to_username: "test2",
+      body: "u1-to-u2",
+    });
+    let m2 = await Message.create({
+      from_username: "test2",
+      to_username: "test1",
+      body: "u2-to-u1",
+    });
+  });
 
-//   test('can get messages from user', async function () {
-//     let m = await User.messagesFrom("test1");
-//     expect(m).toEqual([{
-//       id: expect.any(Number),
-//       body: "u1-to-u2",
-//       sent_at: expect.any(Date),
-//       read_at: null,
-//       to_user: {
-//         username: "test2",
-//         first_name: "Test2",
-//         last_name: "Testy2",
-//         phone: "+14155552222",
-//       }
-//     }]);
-//   });
+  test("can get messages from user", async function () {
+    let m = await User.messagesFrom("test1");
+    expect(m).toEqual([
+      {
+        id: expect.any(Number),
+        body: "u1-to-u2",
+        sent_at: expect.any(Date),
+        read_at: null,
+        to_user: {
+          username: "test2",
+          first_name: "Test2",
+          last_name: "Testy2",
+          phone: "+14155552222",
+        },
+      },
+    ]);
+  });
 
-//   test('can get messages to user', async function () {
-//     let m = await User.messagesTo("test1");
-//     expect(m).toEqual([{
-//       id: expect.any(Number),
-//       body: "u2-to-u1",
-//       sent_at: expect.any(Date),
-//       read_at: null,
-//       from_user: {
-//         username: "test2",
-//         first_name: "Test2",
-//         last_name: "Testy2",
-//         phone: "+14155552222",
-//       }
-//     }]);
-//   });
-// });
+  //   test('can get messages to user', async function () {
+  //     let m = await User.messagesTo("test1");
+  //     expect(m).toEqual([{
+  //       id: expect.any(Number),
+  //       body: "u2-to-u1",
+  //       sent_at: expect.any(Date),
+  //       read_at: null,
+  //       from_user: {
+  //         username: "test2",
+  //         first_name: "Test2",
+  //         last_name: "Testy2",
+  //         phone: "+14155552222",
+  //       }
+  //     }]);
+  //   });
+});
 
 afterAll(async function () {
   await db.end();
