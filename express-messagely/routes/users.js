660c5368e8c8f686901f6959f48807b3e10a52cd
Sara Strong <sara.n.strong@gmail.com>
2021-02-15 15:20:52 -0500

    Get all users route

--- express-messagely/routes/users.js
+++ express-messagely/routes/users.js
@@ -1,16 +1,29 @@
+const Router = require("express").Router;
+const User = require("../models/user");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+
+const router = new Router();
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
 
+router.get("/", ensureLoggedIn, async function (req, res, next) {
+  try {
+    let users = await User.all();
+    return res.json({ users });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
-
 
 /** GET /:username/to - get messages to user
  *
@@ -22,7 +35,6 @@
  *
  **/
 
-
 /** GET /:username/from - get messages from user
  *
  * => {messages: [{id,
@@ -32,3 +44,5 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+
+module.exports = router;
