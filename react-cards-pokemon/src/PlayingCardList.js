15e058b5eecb3bdb50edd512560871bf1b92a8f8
Sara Strong <sara.n.strong@gmail.com>
2021-04-19 14:59:06 -0400

    Step 3 complete

--- react-cards-pokemon/src/PlayingCardList.js
+++ react-cards-pokemon/src/PlayingCardList.js
@@ -3,17 +3,15 @@ import uuid from "uuid";
 import axios from "axios";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
+import { useAxios } from "./hooks";
 
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function CardTable() {
-  const [cards, setCards] = useState([]);
-  const addCard = async () => {
-    const response = await axios.get(
-      "https://deckofcardsapi.com/api/deck/new/draw/"
-    );
-    setCards(cards => [...cards, { ...response.data, id: uuid() }]);
-  };
+  const [cards, addCard] = useAxios(
+    "https://deckofcardsapi.com/api/deck/new/draw/"
+  );
+
   return (
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
@@ -21,7 +19,7 @@ function CardTable() {
         <button onClick={addCard}>Add a playing card!</button>
       </div>
       <div className="PlayingCardList-card-area">
-        {cards.map(cardData => (
+        {cards.map((cardData) => (
           <PlayingCard key={cardData.id} front={cardData.cards[0].image} />
         ))}
       </div>
--- react-cards-pokemon/src/hooks.js
+++ react-cards-pokemon/src/hooks.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import uuid from "uuid";
 
 const useFlip = () => {
   const [isFacingUp, setIsFacingUp] = useState(true);
@@ -8,4 +10,21 @@ const useFlip = () => {
   return [isFacingUp, flipCard];
 };
 
-export default useFlip;
+/* In the PlayingCardList component, we initialize an empty array in state, and add to it via an AJAX request we make with axios. Since we use axios in a few components, let’s move this logic into a function called useAxios.
+
+useAxios should take in a URL, and similar to useState, it should return an array with two elements. The first element is an array of data obtained from previous AJAX requests (since we will add to this array, it’s a piece of state). The second element is a function that will add a new object of data to our array.
+
+Once you’ve written this hook, refactor PlayingCardList to use this custom hook. (Don’t worry about PokeDex for now - that’s coming in the next part! **/
+
+const useAxios = (url) => {
+  const [cards, setCards] = useState([]);
+  const addCard = async () => {
+    const response = await axios.get(
+      "https://deckofcardsapi.com/api/deck/new/draw/"
+    );
+    setCards((cards) => [...cards, { ...response.data, id: uuid() }]);
+  };
+  return [cards, addCard];
+};
+
+export { useFlip, useAxios };
