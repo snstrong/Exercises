f88978934151c85387db31f32ffb326f6fa56362
Sara Strong <sara.n.strong@gmail.com>
2021-06-09 17:31:13 -0400

    sortedFrequency passes test cases

--- prob-divide-conquer/sorted-frequency.js
+++ prob-divide-conquer/sorted-frequency.js
@@ -1,5 +1,36 @@
-function sortedFrequency() {
+/**
+ * Given a sorted array and a number,
+ * write a function called sortedFrequency
+ * that counts the occurrences of the number in the array.
+ *
+ * Examples:
+ * sortedFrequency([1,1,2,2,2,2,3],2) // 4
+ * sortedFrequency([1,1,2,2,2,2,3],3) // 1
+ * sortedFrequency([1,1,2,2,2,2,3],1) // 2
+ * sortedFrequency([1,1,2,2,2,2,3],4) // -1
+ */
 
+function sortedFrequency(arr, num) {
+  let leftIdx = 0;
+  let rightIdx = arr.length - 1;
+  if (arr[leftIdx] > num || arr[rightIdx] < num) return -1;
+  let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+  // find first and last occurence of num in array, if any
+  while (arr[leftIdx] < num || arr[rightIdx] > num) {
+    if (arr[middleIdx] < num && arr[leftIdx] < num) {
+      leftIdx = middleIdx + 1;
+    } else if (arr[middleIdx] > num && arr[rightIdx] > num) {
+      rightIdx = middleIdx - 1;
+    } else if (arr[middleIdx] === num && arr[leftIdx] !== num) {
+      leftIdx++;
+    } else if (arr[middleIdx] === num && arr[rightIdx] !== num) {
+      rightIdx--;
+    } else if (arr[leftIdx] > num || arr[rightIdx] < num) {
+      return -1;
+    }
+    middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+  }
+  return rightIdx - leftIdx + 1;
 }
 
-module.exports = sortedFrequency
+module.exports = sortedFrequency;
