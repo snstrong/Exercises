ec9d3f6d38623b91062e669512384312fab492a7
Sara Strong <sara.n.strong@gmail.com>
2021-06-09 16:54:31 -0400

    countZeroes passes test cases

--- prob-divide-conquer/count-zeroes.js
+++ prob-divide-conquer/count-zeroes.js
@@ -1,5 +1,22 @@
-function countZeroes() {
-  
+/* Given an array of 1s and 0s which has all 1s first followed by all 0s,
+ *  write a function called countZeroes
+ *  which returns the number of zeroes in the array.
+ * E.g, countZeroes([1,1,1,1,0,0]) // 2
+ **/
+function countZeroes(arr) {
+  let rightIdx = arr.length - 1;
+  if (arr[rightIdx] !== 0) return 0;
+  let leftIdx = 0;
+  let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+  while (arr[leftIdx] > 0) {
+    if (arr[middleIdx] > 0) {
+      leftIdx = middleIdx + 1;
+    } else if (arr[middleIdx] === 0) {
+      rightIdx = middleIdx;
+    }
+    middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+  }
+  return arr.length - leftIdx;
 }
 
-module.exports = countZeroes
+module.exports = countZeroes;
