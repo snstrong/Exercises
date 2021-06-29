eb91c8d7116b2a49c2e3070eb5cfcbee1a4f6652
Sara Strong <sara.n.strong@gmail.com>
2021-06-28 22:16:42 -0400

    product passes tests

--- dsa-recursion/recursion.js
+++ dsa-recursion/recursion.js
@@ -1,51 +1,39 @@
 /** product: calculate the product of an array of numbers. */
 
-function product(nums) {
+function product(nums, i = 0) {
+  if (i === nums.length) return 1;
 
+  return nums[i] * product(nums, i + 1);
 }
 
 /** longest: return the length of the longest word in an array of words. */
 
-function longest(words) {
-
-}
+function longest(words) {}
 
 /** everyOther: return a string with every other letter. */
 
-function everyOther(str) {
-
-}
+function everyOther(str) {}
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
-function isPalindrome(str) {
-
-}
+function isPalindrome(str) {}
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {
-
-}
+function findIndex(arr, val) {}
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {
-
-}
+function revString(str) {}
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
-function gatherStrings(obj) {
-
-}
+function gatherStrings(obj) {}
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
 
-function binarySearch(arr, val) {
-
-}
+function binarySearch(arr, val) {}
 
 module.exports = {
   product,
@@ -55,5 +43,5 @@ module.exports = {
   findIndex,
   revString,
   gatherStrings,
-  binarySearch
+  binarySearch,
 };
